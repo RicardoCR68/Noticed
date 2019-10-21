8cf80d97cb51226a9019d1ab81d9b0c29d6db0fd
Ricardo Carvalho Ribeiro <ricardoc6r8@gmail.com>
2019-10-21 17:36:11 -0300

    Cruded the Crud

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,16 +1,46 @@
 class NotesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_note, only: %i[edit update destroy]
+
   def index
+    @notes = Note.all
+  end
+
+  def new
+    @note = Note.new
   end
 
   def create
+    @note = Note.new(note_params)
+    if @note.save
+      redirect_to main_page_path
+    else
+      render new
   end
 
   def edit
   end
 
   def update
+    if @note.update(note_params)
+      redirect_to main_page_path
+    else
+      render edit
+    end
   end
 
-  def delete
+  def destroy
+    @note.destroy
+  end
+
+  private
+
+
+  def set_note
+    @note = Note.find(params[:id])
+  end
+
+  def note_params
+    params.require(:note).permit(:title, :description, :due_date, :priority)
   end
 end
